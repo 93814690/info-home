acdf2b37f81c8d844c2c3087e9a81f365ee17f44
93814690 <93814690@qq.com>
2021-10-05 19:32:09 +0800

    更新fly.push版本号

---
+++ src/test/java/top/liyf/infohome/feign/TestChanifyClient.java
@@ -0,0 +1,27 @@
+package top.liyf.infohome.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import top.liyf.fly.push.api.domain.ChanifyText;
+
+/**
+ * @author liyf
+ * Created in 2021-10-05
+ */
+@SpringBootTest
+class TestChanifyClient {
+
+    @Autowired
+    private ChanifyClient chanifyClient;
+
+    @Test
+    public void testPush() {
+        ChanifyText text = new ChanifyText();
+        text.setTitle("testTitle");
+        text.setText("testText");
+        text.setToken("");
+        text.setInterruptionLevel("active");
+        chanifyClient.text(text);
+    }
+}
