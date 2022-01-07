3067bd2deb272fcc28a717ed6e50a602a6292906
93814690 <93814690@qq.com>
2022-01-07 23:52:45 +0800

    定时获取电影信息

--- src/main/java/top/liyf/infohome/task/MovieTask.java
+++ src/main/java/top/liyf/infohome/task/MovieTask.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import top.liyf.infohome.service.MovieService;
+import top.liyf.infohome.util.RedisConst;
+import top.liyf.redis.service.RedisService;
+
+import java.io.IOException;
 
 /**
  * @author liyf
@@ -14,13 +18,21 @@ import top.liyf.infohome.service.MovieService;
 public class MovieTask {
 
     private final MovieService movieService;
+    private final RedisService redisService;
 
-    public MovieTask(MovieService movieService) {
+    public MovieTask(MovieService movieService, RedisService redisService) {
         this.movieService = movieService;
+        this.redisService = redisService;
     }
 
     @Scheduled(cron = "0 30 6 * * ?")
     public void getLatestMovie() throws Exception {
         movieService.getLatestMovie();
+    }
+
+    @Scheduled(cron = "*/15 * * * * ?")
+    public void getMovieInfo() throws IOException {
+        Object dbId = redisService.sPop(RedisConst.MV_INFO_SET);
+        movieService.getMovieByDouban((Integer) dbId);
     }
 }
